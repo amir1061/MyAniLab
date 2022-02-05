71f4ad147333e77ebb28c89552e7c4811f4494ce
Aissam Ouajib <a.ouajib@devotech.co>
2022-02-05 02:22:53 +0100

    Top Anime: keeping tabs alive

--- lib/UI/Views/top_anime_view.dart
+++ lib/UI/Views/top_anime_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myanilab/Core/Providers/top_anime_provider.dart';
-import 'package:myanilab/UI/Widgets/anime_list.dart';
-import 'package:provider/provider.dart';
+import 'package:myanilab/UI/Widgets/top_anime_list.dart';
 
 class TopAnimeView extends StatelessWidget {
   const TopAnimeView({Key? key}) : super(key: key);
@@ -44,27 +42,4 @@ class TopAnimeView extends StatelessWidget {
       ),
     );
   }
-}
-
-class TopAnimeList<T> extends StatefulWidget {
-  final String type;
-  const TopAnimeList({Key? key, required this.type}) : super(key: key);
-
-  @override
-  _TopAnimeListState createState() => _TopAnimeListState();
-}
-
-class _TopAnimeListState extends State<TopAnimeList>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return ChangeNotifierProvider(
-      create: (_) => TopAnimeProvider(rankingType: widget.type),
-      child: AnimeList<TopAnimeProvider>(),
-    );
-  }
-
-  @override
-  bool get wantKeepAlive => true;
 }
